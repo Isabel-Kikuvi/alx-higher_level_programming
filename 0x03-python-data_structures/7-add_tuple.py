88a23d15f821859c3e56d5114cf5373b074ceb0a
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-05-15 00:55:24 -0700

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
+
+    a, b = tuple_a[:2]
+    c, d = tuple_b[:2]
+
+    result_a = a + c
+    result_b = b + d
+
+    return(result_a, result_b)

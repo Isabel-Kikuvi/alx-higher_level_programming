56671d032c03fa56f164517365f3339f82f8afcc
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-05-29 13:49:42 -0700

    updates

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,10 @@ import sys
 
 
 def safe_print_integer_err(value):
+    var_int = True
     try:
         print("{:d}".format(value))
-        return True
-    except ValueError as e:
+    except Exception as e:
         print("Exception: ", e, file=sys.stderr)
-        return False
+        var_int = False
+    return var_int

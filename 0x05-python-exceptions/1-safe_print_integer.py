d09bcf551b34b083dd380c9f98a9ea11a71b868f
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-05-29 12:36:40 -0700

    updates

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{}".format(value))
-        return True
-    except:
+    except (TypeError, ValueError):
         return False
+    else:
+        return True

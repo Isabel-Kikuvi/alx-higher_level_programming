e1703fe7a5b57cdc42d141e27fe7978ebb65eb30
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-05-29 12:14:51 -0700

    updates

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range (x):
             print(my_list[i], end=" ")
             idx += 1
-    except IndexError:
+    except (IndexError, TypeError, ValueError):
         pass
-    finally:
-        print()
-        return idx
+    print()
+    return idx

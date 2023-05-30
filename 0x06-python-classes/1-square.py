8befb523cce5ecc9b165a4ce643174188aaea743
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-05-30 08:21:48 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define class Square"""
+
+
+class Square:
+    """Reps a square"""
+
+    def __init__(self, size=0):
+        """Initializes a square.
+        Args:
+        size (int): Size of new square.
+        """
+
+        self.__size = size

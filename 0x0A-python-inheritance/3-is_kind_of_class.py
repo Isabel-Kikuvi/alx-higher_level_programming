d53e4a8de9c86c0cbaee009b668a6c81dcd9c229
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-06-12 11:47:23 -0700

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module conatins the func is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True if obj is an instance of a_class, else False"""
+    return isinstance(obj, a_class)

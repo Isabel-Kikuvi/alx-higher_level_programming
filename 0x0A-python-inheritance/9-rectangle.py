e36025dcf1d604bd65cd87f78a1965bffd959098
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-06-12 12:51:35 -0700

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validates value"""
-        if type(value) != int:
+        if type(value) !=  int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
@@ -26,3 +26,11 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """calculate the area"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """declaring the print"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))

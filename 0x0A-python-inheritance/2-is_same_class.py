059a590fb67895096910a0b4dfa0e669a0ea0457
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-06-12 11:42:50 -0700

    refactoring code

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module contains func is_kind_of_class"""
+"""Module contains func is_same_class"""
 
 
-def is_kind_of_class(obj, a_class):
+def is_same_class(obj, a_class):
     """returns true if obj is of same class else, False"""
     return type(obj) == a_class

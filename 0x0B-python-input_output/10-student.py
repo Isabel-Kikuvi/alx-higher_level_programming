c3ad3c291ebad1e21ce8fc2dfca784bb2a6a3d01
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-06-13 15:42:06 -0700

    updates

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Module contains class Student"""
+"""Contains the clas "Student"""
 
 
 class Student:
-    """represents a student"""
+    """Representation of a student"""
     def __init__(self, first_name, last_name, age):
-        """initiates the class student"""
+        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retreives dict rep of student"""
-          if attrs is None:
+        """returns a dictionary representation of a Student instance
+        with specified attributes"""
+        if attrs is None:
             return self.__dict__
         new_dict = {}
         for a in attrs:

9236e8f21b3d08111f6a018023f19aadd93bc5bc
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-06-19 10:48:37 -0700

    updates.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,9 +37,9 @@ class Rectangle(Base):
         """sets the height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-
         if value <= 0:
             raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
@@ -65,10 +65,8 @@ class Rectangle(Base):
         """sets y"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-
         if value < 0:
             raise ValueError("y must be >= 0")
-
         self.__y = value
 
     def area(self):

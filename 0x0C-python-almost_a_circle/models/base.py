9731d067a442d17e7977bfb47b9db6c5d5b9d3ad
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-06-19 11:16:12 -0700

    json loads

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,11 @@ class Base:
             else:
                 list_dict = [i.to_dictionary() for i in list_objs]
                 f.write(Base.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns a list from Json string"""
+        if json_string is None or json_string == "[]":
+            return "[]"
+        else:
+            return json.loads(json_string)

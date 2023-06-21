d8f18ba4dcfa3b3e1149fc662043444ababa9558
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-06-21 07:37:46 -0700

    updates

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,22 @@ class Rectangle(Base):
     """initiatizes a rectagle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class constructor"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__width = width
         self.__height = height
         self.__x = x
@@ -122,7 +138,7 @@ class Rectangle(Base):
                 elif key == "y":
                     self.y = value
                 else:
-                    raise ValueError("Invalid attribut: {key}")
+                    raise ValueError("Invalid attribute: {key}")
 
     def to_dictionary(self):
         """Returns the dict representation of a rectagle"""

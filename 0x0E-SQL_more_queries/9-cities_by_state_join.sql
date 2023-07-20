feb6e92d12bd5dbc678805988b720c4cdafc19ed
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-07-20 13:27:56 -0700

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,9 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa.
 
-SELECT	c.id,
-	c.name,
-	s.name
-FROM cities c
-JOIN states s
-ON c.id = s.id
-ORDER BY c.id ASC
+SELECT	cities.id,
+	cities.name,
+	states.name
+FROM cities
+JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;

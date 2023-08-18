e11b3a48eabb0d50b837a9e16c530f5b752ca4f8
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-08-17 23:43:00 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+
+"""prints value passed as args"""
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    curr = conn.cursor()
+    curr.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+            ORDER BY states.id ASC".format(argv[4]))
+    
+    myresult = curr.fetchall()
+    for row in myresult:
+        print(row)

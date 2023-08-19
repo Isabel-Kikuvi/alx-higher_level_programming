a2be80c7e73a0a6633d308863e0525cc932f99a1
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-08-19 13:39:06 -0700

    9-model_state_filter_a.py

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A script that lists states which contain 'a'
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """access dbase to list states with 'a'"""
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.contains('a'))
+    if states is not None:
+        for instance in states:
+            print('{0}: {1}'.format(instance.id, instance.name))

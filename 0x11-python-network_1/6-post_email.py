d9681c454b4263ddfe4295628a80e0bb83c38767
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-09-01 14:16:22 -0700

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,10 +4,11 @@ import sys
 import requests
 
 
-if __name == "__main__":
+if __name__ == "__main__":
 
     url = sys.argv[1]
     email = sys.argv[2]
+
 
     r = requests.post(url, data={'email': email})
     print(r.text)
